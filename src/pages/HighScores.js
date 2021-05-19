ab02d201d79937ed4a9f9d6f2e1b2cdaba246e5b
Micheal Sihavong <1micheal.sihavong2@gmail.com>
2021-05-19 00:27:25 -0700

    add fetch to airtable and display on /HighScores rout

--- src/pages/HighScores.js
+++ src/pages/HighScores.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ScoreLI, ScoresList } from '../styled/HighScores';
 
 export default function HighScores() {
+    // cache the scores with hook
+    const [highScores, setHighScore] = useState([]);
+
+    // use fetchAPI to call getHighScores function
+    useEffect(() => {
+        const loadHighScores = async () => {
+            try {
+                const res = await fetch('/.netlify/functions/getHighScores');
+                const scores = await res.json();
+                setHighScore(scores);
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        loadHighScores();
+    }, []);
+    // display scores
     return (
         <div>
             <h1>High Scores</h1>
+            <ScoresList>
+                {highScores.map((score) => {
+                    return (
+                        <ScoreLI key={score.id}>
+                            {score.fields.name} - {score.fields.score}
+                        </ScoreLI>
+                    );
+                })}
+            </ScoresList>
         </div>
     );
 }
